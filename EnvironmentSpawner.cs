d9c568d223e540b1ba64650fc30705a42c18a42e
Jack Hancock <77704393+JackalGame@users.noreply.github.com>
2021-02-14 22:31:42 +0000

    2 scripts added

---
+++ EnvironmentSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] slopePrefabs;
+    [Tooltip("where do we want the block to spawn")][SerializeField] Vector2 lastPos;
+    [SerializeField] Transform parent;
+    [SerializeField] int numOfInitialBlocks = 4;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuildInitialSlope();
+    }
+
+    private void BuildInitialSlope()
+    {
+        for(int i = 0; i < numOfInitialBlocks; i++)
+        {
+            SpawnSlopePart();
+        }
+    }
+
+    public void SpawnSlopePart() // method also called when last slope part is destroyed.
+    {
+        Vector2 spawnPos = new Vector2(lastPos.x, lastPos.y);
+        var slopeToSpawn = slopePrefabs[Random.Range(0, slopePrefabs.Length)];
+        GameObject slope = Instantiate(slopeToSpawn, spawnPos, transform.rotation);
+        slope.transform.parent = parent; // Keeps object under the relevant tab within the Hierarchy.
+        Transform endPoint = slope.transform.GetChild(1);
+        lastPos = endPoint.transform.position;
+    }
+
+}
---
+++ TimeManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{  
+    [Header("Countdown Timer")]
+    [SerializeField] float timeLeft = 20f;
+    [SerializeField] float restoreTimeAmount = 20f;
+
+    [Header("Canvas")]
+    [SerializeField] GameObject gameOverCanvas;
+    [SerializeField] Text countdownText;
+
+    private static TimeManager _instance;
+
+    private ColorGrading colorGradingLayer;
+    private Grain grainLayer;
+
+    public static TimeManager Instance
+    {
+        get
+        {
+            if (_instance == null) _instance = FindObjectOfType<TimeManager>();
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null) Destroy(gameObject);
+        else _instance = this;
+
+        PostProcessVolume camVol = Camera.main.GetComponent<PostProcessVolume>();
+        camVol.profile.TryGetSettings(out colorGradingLayer);
+        camVol.profile.TryGetSettings(out grainLayer);
+
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DecreaseTime();
+        ControlCameraEffect();
+    }
+
+    private void DecreaseTime()
+    {
+        timeLeft -= Time.deltaTime;
+        if(timeLeft < 0)
+        {
+            countdownText.text = "Oh no! The Nothing has taken over.";
+            timeLeft = 0;
+            gameOverCanvas.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            countdownText.text = timeLeft.ToString();
+        }
+    }
+
+    public void RestoreTime() // We can call this from a pickup.
+    {
+        timeLeft = restoreTimeAmount;
+    }
+
+    private void ControlCameraEffect() // Camera fades to GreyScale the closer the score gets to 0.
+    {
+        if (timeLeft <= 20)
+        {
+            colorGradingLayer.enabled.value = true;
+            var saturation = timeLeft * 5 - 100;
+            colorGradingLayer.saturation.value = saturation;
+            
+            if(timeLeft <= 1)
+            {
+                var grain = 1 - timeLeft;
+                grainLayer.enabled.value = true;
+                grainLayer.intensity.value = grain;
+            }
+
+        }
+        else
+        {
+            colorGradingLayer.enabled.value = false;
+            grainLayer.enabled.value = false;
+        }
+    }
+}
